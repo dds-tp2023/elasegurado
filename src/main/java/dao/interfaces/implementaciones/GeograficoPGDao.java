597cbd1a175beaaba6b647285896f224937ca0be
yamilrafart <yamilrafart@hotmail.com>
2023-11-21 15:20:11 -0300

    Add funcionalidad de recuperar provincias y localidades

--- src/main/java/dao/interfaces/implementaciones/GeograficoPGDao.java
+++ src/main/java/dao/interfaces/implementaciones/GeograficoPGDao.java
@@ -1,7 +1,40 @@
 package dao.interfaces.implementaciones;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
 import dao.interfaces.GeograficoDao;
+import dominio.Localidad;
+import dominio.Provincia;
+import dto.LocalidadDTO;
+import dto.ProvinciaDTO;
+import utils.HibernateUtil;
 
 public class GeograficoPGDao implements GeograficoDao {
-
+	@Override
+	public List<ProvinciaDTO> getAllProvinciasConLocalidades() {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Query<Provincia> query = session.createQuery("from Provincia", Provincia.class);
+		List<Provincia> provincias = query.getResultList();
+		List<ProvinciaDTO> provinciasDTO = new ArrayList<ProvinciaDTO>();
+		for (Provincia p : provincias) {
+			ProvinciaDTO pDto = new ProvinciaDTO();
+			pDto.setId(p.getId());
+			Query<Provincia> query2 = session.createQuery("select p from Provincia p join fetch p.localidades where p.id = :id", Provincia.class);
+			query2.setParameter("id", p.getId());
+			Provincia pr = query2.getSingleResult();
+			List<LocalidadDTO> listaLocalidadesDTO = new ArrayList<LocalidadDTO>();
+			for(Localidad l : pr.getLocalidades()) {
+				LocalidadDTO lDto = new LocalidadDTO(l.getId(), l.getNombreLocalidad());
+				listaLocalidadesDTO.add(lDto);
+			}
+			pDto.setLocalidades(listaLocalidadesDTO);
+			pDto.setNombreProvincia(p.getNombreProvincia());
+			provinciasDTO.add(pDto);
+		}
+		session.close();
+		return provinciasDTO;
+	}
 }
--- src/main/java/gestores/GestorGeografico.java
+++ src/main/java/gestores/GestorGeografico.java
@@ -1,12 +1,21 @@
 package gestores;
 
+import java.util.List;
+import dao.factory.FactoryDao;
+import dao.interfaces.GeograficoDao;
+import dto.ProvinciaDTO;
+
 public final class GestorGeografico {
 	private static GestorGeografico instancia;
+	private FactoryDao factory = FactoryDao.getFactory(FactoryDao.PG_FACTORY);
+	private GeograficoDao geograficoDao = factory.getGeograficoDao();
 	
-	private GestorGeografico() {}
+	private GestorGeografico() {
+	}
 	
 	public synchronized static GestorGeografico getInstancia() {
-		if(instancia == null) instancia = new GestorGeografico();
+		if (instancia == null)
+			instancia = new GestorGeografico();
 		return instancia;
 	}
 
@@ -14,4 +23,10 @@ public final class GestorGeografico {
 	public Object clone() throws CloneNotSupportedException {
 		throw new CloneNotSupportedException();
 	}
+	
+	public List<ProvinciaDTO> listaProvinciaComboBox() {
+		List<ProvinciaDTO> provincias = null;
+		return provincias = geograficoDao.getAllProvinciasConLocalidades();
+	}
+	
 }
